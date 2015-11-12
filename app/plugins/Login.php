d644ff965180e96facf019d0d6e4fdaa9b2a2115
Ares <admin@phpdr.net>
2015-11-12 12:02:36 +0800

    update

--- app/plugins/Login.php
+++ app/plugins/Login.php
@@ -1,6 +1,6 @@
 <?php
 class LoginPlugin extends Yaf_Plugin_Abstract{
-	function preDispatch($request, $response){
+	function preDispatch(Yaf_Request_Abstract $request,Yaf_Response_Abstract $response){
 		$controllerName=strtolower($request->getControllerName());
 		if(0===strpos($controllerName,'doc')){
 			$model = LoginModel::getInstance ();
