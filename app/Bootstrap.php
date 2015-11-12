6d975e34254a578652b3615e31ae010c169abd33
Ares <admin@phpdr.net>
2015-11-12 22:11:03 +0800

    update

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 class Bootstrap extends Yaf_Bootstrap_Abstract {
 	function _initPlugin(Yaf_Dispatcher $dispatcher) {
+		Yaf_Loader::import ( 'vendor/autoload.php' );
 		$dispatcher->registerPlugin ( new Plugin_Init () );
 		$dispatcher->registerPlugin ( new Plugin_Smarty () );
 		$dispatcher->registerPlugin ( new LoginPlugin () );
