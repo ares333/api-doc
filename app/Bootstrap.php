3da07e422cb57f6010cae0a510bec90daf40f79d
Ares <aresrr@qq.com>
2017-04-19 14:09:21 +0800

    update

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -5,19 +5,29 @@ use Yaf\Loader;
 use Ares333\YafLib\Plugin\PHPConfig;
 use Ares333\YafLib\Plugin\Module;
 use Ares333\YafLib\SmartyView;
-class Bootstrap extends Bootstrap_Abstract {
-	function _initPlugin(Dispatcher $dispatcher) {
-		Loader::import ( 'vendor/autoload.php' );
-		$dispatcher->registerPlugin ( new PHPConfig () );
-		$dispatcher->registerPlugin ( new LoginPlugin () );
-		$dispatcher->registerPlugin ( new Module ( array (
-				Module::TYPE_CLI => array (
-						'enable' => true
-				)
-		) ) );
-	}
-	function _initView(Dispatcher $dispatcher) {
-		$view = new SmartyView ();
-		$dispatcher->setView ( $view );
-	}
+use Ares333\YafLib\Helper\Error;
+
+class Bootstrap extends Bootstrap_Abstract
+{
+
+    function _initPlugin(Dispatcher $dispatcher)
+    {
+        Loader::import('vendor/autoload.php');
+        $dispatcher->registerPlugin(new PHPConfig());
+        $dispatcher->registerPlugin(new LoginPlugin());
+        $dispatcher->registerPlugin(
+            new Module(
+                array(
+                    Module::TYPE_CLI => array(
+                        'enable' => true
+                    )
+                )));
+        Error::error2exception();
+    }
+
+    function _initView(Dispatcher $dispatcher)
+    {
+        $view = new SmartyView();
+        $dispatcher->setView($view);
+    }
 }
