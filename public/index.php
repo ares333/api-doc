d1e8a74332fb5cb41a45827660ff895b611d0bd8
Ares <aresrr@qq.com>
2018-06-22 13:35:26 +0800

    mv conf directory to app directory.

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 use Yaf\Application;
 $appPath = dirname(__DIR__) . '/app';
-$app = new Application($appPath . '/../conf/app.ini');
+$app = new Application($appPath . '/conf/app.ini');
 $app->setAppDirectory($appPath)
     ->bootstrap()
     ->run();
